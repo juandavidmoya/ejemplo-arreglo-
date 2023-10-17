e249b98fd1ad23001351e63a81d4aad6f0a3ae23
ingridcarolina13 <110685836+ingridcarolina13@users.noreply.github.com>
2023-10-17 17:30:20 -0500

    mmmm

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main (String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
